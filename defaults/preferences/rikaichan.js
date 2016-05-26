ffad5ecca87878ab015deea57c69dc2c5376da6d
Tomás Godoi <tsmgodoi@gmail.com>
2016-05-26 14:27:52 -0300

    Extracting the Rikaisama version 22.9.3

---
+++ defaults/preferences/rikaichan.js
@@ -0,0 +1,123 @@
+pref('extensions.rikaisama.css', 'blue');
+pref('extensions.rikaisama.enmode', 1);
+pref('extensions.rikaisama.highlight', true);
+pref('extensions.rikaisama.selinlb', true);
+pref('extensions.rikaisama.bottomlb', false);
+pref('extensions.rikaisama.resizedoc', false);
+pref('extensions.rikaisama.sticon', true);
+pref('extensions.rikaisama.minihelp', true);
+pref('extensions.rikaisama.volume', 100);
+pref('extensions.rikaisama.autoplayaudio', false);
+pref('extensions.rikaisama.enablenoaudioclip', true);
+pref('extensions.rikaisama.opacity', 100);
+pref('extensions.rikaisama.roundedcorners', true);
+pref('extensions.rikaisama.mergedivs', false); // Let Rikaisama span DIV elements when performing a lookup.
+
+pref('extensions.rikaisama.startlookupbar', false);
+pref('extensions.rikaisama.startepwing', false);
+pref('extensions.rikaisama.startsanseido', false);
+pref('extensions.rikaisama.startsticky', false);
+pref('extensions.rikaisama.startsupersticky', false);
+
+pref('extensions.rikaisama.cmtoggle', true);
+pref('extensions.rikaisama.cmlbar', false);
+pref('extensions.rikaisama.tmtoggle', true);
+pref('extensions.rikaisama.tmlbar', true);
+
+pref('extensions.rikaisama.nopopkeys', false);
+pref('extensions.rikaisama.kbalternateview', '65');
+pref('extensions.rikaisama.kbstickypopup', '75');
+pref('extensions.rikaisama.kbmovepopupdown', '89');
+pref('extensions.rikaisama.kbcopytoclipboard', '67');
+pref('extensions.rikaisama.kbsavetofile', '83');
+pref('extensions.rikaisama.kbsavetofilekana', '88');
+pref('extensions.rikaisama.kbhideshowdefinitions', '68');
+pref('extensions.rikaisama.kbpreviouscharacter', '66');
+pref('extensions.rikaisama.kbnextcharacter', '77');
+pref('extensions.rikaisama.kbnextword', '78');
+pref('extensions.rikaisama.kbjdicaudio', '70');
+pref('extensions.rikaisama.kbsanseidomode', '79');
+pref('extensions.rikaisama.kbepwingmode', '80');
+pref('extensions.rikaisama.kbrealtimeimport', '82');
+pref('extensions.rikaisama.kbrealtimeimportkana', '84');
+pref('extensions.rikaisama.kbsuperstickymode', '85');
+pref('extensions.rikaisama.kbeditnotes', '74');
+pref('extensions.rikaisama.kbepwingnextdic', '61');
+pref('extensions.rikaisama.kbepwingprevdic', '173');
+pref('extensions.rikaisama.kbepwingnextentry', '221');
+pref('extensions.rikaisama.kbepwingpreventry', '219');
+
+pref('extensions.rikaisama.toggle.key', 'INSERT');
+pref('extensions.rikaisama.toggle.mod', 'alt');
+
+pref('extensions.rikaisama.lbar.key', 'DELETE');
+pref('extensions.rikaisama.lbar.mod', 'alt');
+
+pref('extensions.rikaisama.dpriority', '');
+pref('extensions.rikaisama.wpos', true);
+pref('extensions.rikaisama.wpop', true);
+pref('extensions.rikaisama.wmax', 10);
+pref('extensions.rikaisama.namax', 20);
+pref('extensions.rikaisama.showfreq', false);
+pref('extensions.rikaisama.showpitchaccent', false);
+pref('extensions.rikaisama.hidepitchaccentpos', true);
+
+pref('extensions.rikaisama.kindex', 'COMP,H,L,E,DK,N,V,Y,P,IN,I,U');
+
+pref('extensions.rikaisama.epwingdiclist', '');
+pref('extensions.rikaisama.epwingfallback', 'jmdict');
+pref('extensions.rikaisama.epwingmaxlines', 15);
+pref('extensions.rikaisama.epwingmaxentries', 5);
+pref('extensions.rikaisama.epwingshowconjugation', true);
+pref('extensions.rikaisama.epwingshowdicnum', true);
+pref('extensions.rikaisama.epwingshowtitle', true);
+pref('extensions.rikaisama.epwingshowshorttitle', true);
+pref('extensions.rikaisama.epwingstripnewlines', false);
+pref('extensions.rikaisama.epwingshowallentries', false);
+pref('extensions.rikaisama.epwingappendjmdict', false);
+pref('extensions.rikaisama.epwingaddcolorandpitch', true);
+pref('extensions.rikaisama.epwingforceparse', false);
+pref('extensions.rikaisama.epwingsearchnextlongest', false);
+pref('extensions.rikaisama.epwingremoveregex', '');
+pref('extensions.rikaisama.epwing_apply_remove_regex_when_saving', true);
+pref('extensions.rikaisama.epwingusewine', false);
+
+pref('extensions.rikaisama.sfile', '');
+pref('extensions.rikaisama.audiodir', '');
+pref('extensions.rikaisama.saveaudioonplay', false);
+pref('extensions.rikaisama.sfcs', 'utf-8');
+pref('extensions.rikaisama.ubom', true);
+pref('extensions.rikaisama.smaxfe', 1);
+pref('extensions.rikaisama.smaxfk', 1);
+pref('extensions.rikaisama.smaxce', 7);
+pref('extensions.rikaisama.smaxck', 1);
+pref('extensions.rikaisama.snlf', 0);
+pref('extensions.rikaisama.ssep', 'Tab');
+pref('extensions.rikaisama.atags', '');
+pref('extensions.rikaisama.savenotes', '');
+pref('extensions.rikaisama.saveformat', '$d$t$r$t$n');
+
+pref('extensions.rikaisama.vocabknownwordslistfile', '');
+pref('extensions.rikaisama.vocabknownwordslistcolumn', 1);
+pref('extensions.rikaisama.vocabtodowordslistfile', '');
+pref('extensions.rikaisama.vocabtodowordslistcolumn', 1);
+
+pref('extensions.rikaisama.rti_save_format', '$d$t$r$t$n');
+pref('extensions.rikaisama.rtifieldnamestext', 'Expression Reading Meaning');
+pref('extensions.rikaisama.rtisaveaudio', false);
+pref('extensions.rikaisama.rtiudpport', 49600);
+
+pref('extensions.rikaisama.version', '');
+pref('extensions.rikaisama.checkversion', true);
+
+pref('extensions.rikaisama.popdelay', 50);
+pref('extensions.rikaisama.title', true);
+pref('extensions.rikaisama.dpriority', '');
+pref('extensions.rikaisama.hidedef', false);
+pref('extensions.rikaisama.hidex', false);
+//	pref('extensions.rikaisama.sticky', false);
+
+pref('extensions.rikaisama.firsticon', true);
+
+
+
